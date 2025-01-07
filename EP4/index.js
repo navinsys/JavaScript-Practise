deb789a2e83279f4c9f42792d8e07a451f8c14c9
Navinkumar Singh <singhnavinkumar493@gmail.com>
2025-01-07 15:15:21 +0530

    Let const var

---
+++ EP4/index.js
@@ -0,0 +1,20 @@
+//learning let and const in ES6
+
+// const a = 10;
+
+// a = 20;
+
+// console.log(a);
+
+// let 
+// let b = 200;
+// b = 100;
+
+
+// console.log(b);
+
+// var
+
+var c = 1;
+c = 2;
+console.log(c);

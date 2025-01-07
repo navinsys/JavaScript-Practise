01ec22fec86f779a3f588ab489d743f274748a17
Navinkumar Singh <singhnavinkumar493@gmail.com>
2025-01-07 12:29:04 +0530

    Initial commit

---
+++ EP1/index.js
@@ -0,0 +1,11 @@
+const age = 20;
+const name = "John";
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+
+
+const a = 10;
+const b = 20;
+
+console.log(    `Sum of ${a} and ${b} is ${a + b}  `  );
